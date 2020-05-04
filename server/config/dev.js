db2152dc4ce333a303645f6e6900f6eabce9f0cf
Mark Dsouza <markdsouza93@gmail.com>
2020-05-04 20:07:53 +0530

    Update dev.js

--- server/config/dev.js
+++ server/config/dev.js
@@ -5,5 +5,5 @@ module.exports = {
     JWT_SECRET: 'eyJhbGciOiJIUzI1',
     CLOUDINARY_NAME: 'mark93', 
     CLOUDINARY_KEY: '245176693976544', 
-    CLOUDINARY_SECRET: 'BeCZ-BAnc-SHG42P8og2ISNbVa4'
+    CLOUDINARY_SECRET: ''
   }
